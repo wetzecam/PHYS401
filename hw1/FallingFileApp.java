2fe974e757bc3f633b5e2001664338a71e8f309f
Cameron Wetzel <c.wetzel4096@gmail.com>
2020-01-15 14:30:50 -0600

    Hw2

--- hw1/FallingFileApp.java
+++ hw1/FallingFileApp.java
@@ -3,21 +3,25 @@ import java.io.*;
 public class FallingFileApp {
 
 	public static void main(String[] args) {
- 
-	    double y0 = 0.;   
-	    double vy0 = 6.;  
-	    
-	    double t = 0.;    
-	    double dt = 0.04; 
-	    double y = y0;
-	    double vy = vy0;
-	    double g = 9.8;   
+	    int N_step = 30;
+	    double y_min = 10.0;
+	    boolean tf = false; 
+	    while(!tf){
+
+	    	double y0 = 0.;   
+	   	double vy0 = 6.;  
+	   	 
+	   	double t = 0.;    
+	   	double dt = 0.04; 
+	   	double y = y0;
+	   	double vy = vy0;
+	  	double g = 9.8;   
 	      
 	      try{
 	    	  FileWriter fw = new FileWriter("output.txt");
 	    	  BufferedWriter fout = new BufferedWriter(fw);
  
-	    	   for(int n = 0;n<30;n++) {  
+	    	   for(int n = 0;n<35;n++) {  
 	    			      y = y+vy*dt; 
 	    			      vy = vy-g*dt; 
 	    			      t = t+dt;
@@ -32,8 +36,19 @@ public class FallingFileApp {
 		System.out.println("  ");					      
 		System.out.println("All done!");					      
 		System.out.println("final time   = "+t);					      
-		System.out.println("final height = "+y);					      
+		System.out.println("final height = "+y);
+		
+		System.out.println("Num Steps = "+N_step);		
+	    
 
+	    	if(y <= y_min) { 
+			y_min = y; 
+			++N_step; }
+	    	else if(y > y_min) { 
+		    tf = true;
+		    System.out.println("Optimal Num Steps = "+N_step); 
+	    }
+	    } 
 	}
 
 }
